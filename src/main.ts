8fabd3ef94750a998aa986cd3b051a6635762547
Jean Lopez <jeanlopezcortes@gmail.com>
2025-05-25 04:38:32 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'https://watchlist-frontend.netlify.app/*', 
+    origin: '*', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept, Authorization',
   });
